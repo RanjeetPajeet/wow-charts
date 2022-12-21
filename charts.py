23a080cabe6f34ceed474b2604897032195a53df
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-21 14:58:09 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -88,11 +88,11 @@ def plot_price_history(item:str, server:str, faction:str, num_days:int, ma4:bool
     if ma12:
         if hide_original:
             if ma4:
-                chart = chart + alt.Chart(data).mark_line(color="#6d3fc0",strokeWidth=2).encode(
+                chart = chart + alt.Chart(data).mark_line(color="#6d3fc0",strokeWidth=2.1).encode(
                                     x=alt.X("Time", axis=alt.Axis(title="Date")), 
                                     y=alt.Y("12-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims)))
             else:
-                chart = alt.Chart(data).mark_line(color="#6d3fc0",strokeWidth=2).encode(
+                chart = alt.Chart(data).mark_line(color="#6d3fc0",strokeWidth=2.1).encode(
                             x=alt.X("Time", axis=alt.Axis(title="Date")), 
                             y=alt.Y("12-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims)))
         else:
