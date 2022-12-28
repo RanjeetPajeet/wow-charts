daea374c769a109d960c863f6249aa9405d1d7e0
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-28 17:59:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ with st.container():
         #st.markdown("##")
         server_col_compare, faction_col_compare = st.columns(2)
         with server_col_compare:
-            server_compare = st.selectbox("Server", ["Skyfury","Faerlina","Whitemane"], key="server_compare")
+            server_compare = st.selectbox("Compare with", [None,"Skyfury","Faerlina","Whitemane"], key="server_compare")
         with faction_col_compare:
-            faction_compare = st.selectbox("Faction", ["Alliance","Horde"], key="faction_compare")
+            faction_compare = st.selectbox("", [None,"Alliance","Horde"], key="faction_compare")
             
-        compare_with = st.selectbox("Compare with", [None,"A","B","C"])
+        #compare_with = st.selectbox("Compare with", [None,"A","B","C"])
 
     # chart_smoothing = st.select_slider("Smoothing", options=[2,4,8,12,24,48], value=2, help="Amount of smoothing for the chart, in hours.")
 
