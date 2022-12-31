01e4088fd93d8816ffc4f24eae09d820202f8234
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 16:37:32 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -193,6 +193,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
             "4-hour moving average":  pd.Series(prices).rolling( 2).mean(),
             "12-hour moving average": pd.Series(prices).rolling( 6).mean(),
             "24-hour moving average": pd.Series(prices).rolling(12).mean(),
+            "Quantity 12hMAog": pd.Series(data["quantities"]).rolling( 6).mean(),
         }
     )
 
@@ -355,7 +356,8 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
             clip=True,
         ).encode(
             x=alt.X("Time", axis=alt.Axis(title="Date")),
-            y=alt.Y("Quantity 12hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims))
+            y=alt.Y("Quantity 12hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims)),
+            tooltip=["Time", "Quantity 12hMAog"]
         )
         if hide_original:
             if ma4:
