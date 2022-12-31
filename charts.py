8086ff48a2528379b1b095e1bace33b9e6adea09
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 16:43:23 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -257,11 +257,11 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
         maximum = max(prices)
     
     
-    try: chart_ylims = (int(minimum/1.25), int(maximum*1.2))
+    try: chart_ylims = (int(minimum/1.25), int(maximum*1.1))
     except Exception as e:
         chart_ylims = (
             int(min(prices)/1.25),
-            int(max(prices)*1.20),
+            int(max(prices)*1.10),
         )
         st.markdown(f"**Error:** {e}")
     
