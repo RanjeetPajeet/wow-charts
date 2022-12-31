e250ac72da49ee838a635bf2bfea9207913aa765
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 16:32:02 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -355,6 +355,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
             interpolate="monotone",
             line=True,
             strokeOpacity=0.5,
+            clip=True,
         ).encode(
             x=alt.X("Time", axis=alt.Axis(title="Date")),
             y=alt.Y("Quantity 12hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims))
