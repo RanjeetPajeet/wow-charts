b584cad293e72faadc92269361dd04d459d08366
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 16:54:10 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -297,7 +297,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
             color=alt.Gradient(
                 gradient="linear",
                 stops=[alt.GradientStop(color="#83c9ff", offset=0),     # bottom color
-                       alt.GradientStop(color="#0068c9", offset=0.4)],  # top color
+                       alt.GradientStop(color="#0068c9", offset=1)],  # top color
                 x1=1, x2=1, y1=1, y2=0,
             ),
             opacity = 0.5,
@@ -353,7 +353,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
         quantity_line_ma12 = alt.Chart(data).mark_area(
             color=alt.Gradient(
                 gradient="linear",
-                stops=[alt.GradientStop(color="#9670dc", offset=0),     # bottom color
+                stops=[alt.GradientStop(color="#9670dc", offset=0.5),     # bottom color
                        alt.GradientStop(color="#5728ae", offset=1)],  # top color
                 x1=1, x2=1, y1=1, y2=0,
             ),
