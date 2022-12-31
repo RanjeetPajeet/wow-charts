0914a5177584099b43b3638fb3d18414c9abf009
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 15:54:17 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -317,25 +317,45 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
     
 
     if ma12:
-        price_line_ma12 = alt.Chart(data).mark_line(
-            color = "#6029c1",
-            strokeWidth = 2.1,
-        ).encode(
-            x=alt.X("Time", axis=alt.Axis(title="Date")),
-            y=alt.Y("12-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims))
+#         price_line_ma12 = alt.Chart(data).mark_line(
+#             color = "#6029c1",
+#             strokeWidth = 2.1,
+#         ).encode(
+#             x=alt.X("Time", axis=alt.Axis(title="Date")),
+#             y=alt.Y("12-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims))
+#         )
+#         quantity_line_ma12 = alt.Chart(data).mark_area(
+#             color=alt.Gradient(
+#                 gradient="linear",
+#                 stops=[alt.GradientStop(color="#9670dc", offset=0),     # bottom color
+#                        alt.GradientStop(color="#5728ae", offset=0.4)],  # top color
+#                 x1=1, x2=1, y1=1, y2=0,
+#             ),
+#             opacity = 0.5
+#         ).encode(
+#             x=alt.X("Time", axis=alt.Axis(title="Date")),
+#             y=alt.Y("Quantity 12hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims))
+#         )
+        price_line = alt.Chart(data).mark_line(
+                    color="#6d3fc0",strokeWidth=2.2).encode(
+                    x=alt.X("Time", axis=alt.Axis(title="Date")), 
+                    y=alt.Y("24-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims))
         )
-        quantity_line_ma12 = alt.Chart(data).mark_area(
-            color=alt.Gradient(
-                gradient="linear",
-                stops=[alt.GradientStop(color="#9670dc", offset=0),     # bottom color
-                       alt.GradientStop(color="#5728ae", offset=0.4)],  # top color
-                x1=1, x2=1, y1=1, y2=0,
-            ),
-            opacity = 0.5
-        ).encode(
-            x=alt.X("Time", axis=alt.Axis(title="Date")),
-            y=alt.Y("Quantity 12hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims))
+        quantity_line = alt.Chart(data).mark_area(
+                    color=alt.Gradient(
+                        gradient="linear",
+                        stops=[alt.GradientStop(color="#3d9df3", offset=0),
+                               alt.GradientStop(color="#004280", offset=data["Quantity 24hMA"].mean()/data["Quantity 24hMA"].max())],
+                        x1=1,
+                        x2=1,
+                        y1=1,
+                        y2=0,
+                    ),
+                    opacity=0.5,strokeWidth=2).encode(
+                    x=alt.X("Time", axis=alt.Axis(title="Date")),
+                    y=alt.Y("Quantity 24hMA", axis=alt.Axis(title="Quantity"), scale=alt.Scale(domain=chart_ylims))
         )
+        return price_line + quantity_line
         if hide_original:
             if ma4:
                 chart = chart + quantity_line_ma12 + price_line_ma12
