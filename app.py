c6f4b43a24d5046bc1b897677e47771033c8b052
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2022-12-31 19:10:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,9 @@ hide_element("footer", "class", "css-1lsmgbg egzxvld0")
 
 hide_element("a", "href", "https://streamlit.io/cloud")
 hide_element("div", "class", "viewerBadge_link__1S137")
+hide_element("a", "target", "_blank")
+hide_element("a", "rel", "noopener noreferrer")
+hide_element("a", "class", "viewerBadge_container__1QSob")
 
 
 
