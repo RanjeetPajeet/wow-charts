3ae1c644434eb637372ac6489fcb83233dd15d08
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-03 04:23:08 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -404,7 +404,9 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
             if ma4:
                 chart = chart + quantity_line_ma12 + price_line_ma12
             else:
-                chart = quantity_line_ma12 + price_line_ma12
+                chart = mouseover_stuff(quantity_line_ma12, price_line_ma12)
+                return chart
+#                 chart = quantity_line_ma12 + price_line_ma12
         else:
             chart = chart + quantity_line_ma12 + price_line_ma12
     
