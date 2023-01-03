3de34f1197f7d732982f5fed19dd322537460277
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-03 04:13:09 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -351,7 +351,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
     
 
     if ma12:
-        price_line_ma12 = alt.Chart(data).mark_line(
+        price_line_ma12 = alt.Chart(data).mark_line(interpolate="basis",
             color = "#6029c1",
             strokeWidth = 2.1,
         ).encode(
