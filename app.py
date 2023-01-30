8a8efb830852539191e8f7e736e3ec4757a0e66d
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-30 05:51:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ st.set_page_config(
     page_title = "AH Prices",
 )
 
+# Hides the "Made with Streamlit" footer element
+hide_element("footer", "class", "css-1lsmgbg egzxvld0")
+
+
 st.title("Auction House Data")
 st.markdown("---")
 
@@ -29,7 +33,6 @@ with st.container():
     
     with item_col:
         item = st.text_input("Item name", "Saronite Ore")
-    
     with days_col:
         num_days = st.number_input("Number of days", 1, 365, 90)
 
@@ -40,7 +43,6 @@ with st.container():
     
     with server_col:
         server = st.selectbox("Server", ["Skyfury","Atiesh","Faerlina","Whitemane"])
-    
     with faction_col:
         if server == "Skyfury" or server == "Atiesh":
             faction = st.selectbox("Faction", ["Alliance","Horde"])
@@ -54,9 +56,11 @@ with st.container():
 
     
     
+    
     chart_type = st.selectbox("Chart type", ["Price & Quantity","Price","Price & Region Price"])
 
 
+    
     
     if chart_type == "Price":
         server_col_compare, faction_col_compare = st.columns(2)
@@ -70,8 +74,6 @@ with st.container():
                     faction_compare = st.selectbox(" ", [f for f in ["Alliance","Horde"] if f != faction], key="faction_compare", disabled=True)
                 elif server_compare == "Skyfury" or server_compare == "Atiesh":
                     faction_compare = st.selectbox(" ", ["Alliance","Horde"], key="faction_compare")
-#                 elif server_compare == "Atiesh":
-#                     faction_compare = st.selectbox(" ", ["Alliance","Horde"], key="faction_compare")
                 else:
                     faction_compare = st.selectbox(" ", ["Horde","Alliance"], key="faction_compare")
             else:
@@ -86,7 +88,7 @@ with st.container():
     
 
     
-#     ma_col4, ma_col12, ma_col24, hide_og_col = st.columns(4)
+    
     ma_col4, ma_col12, ma_col24, ma_col48, hide_og_col = st.columns(5)
     with ma_col4:
         ma4 = st.checkbox("4 hour", value=False, key="ma4_checkbox")
@@ -98,12 +100,14 @@ with st.container():
         ma48 = st.checkbox("48 hour", value=False, key="ma48_checkbox")
     with hide_og_col:
         hide_original = st.checkbox("Hide raw", value=True, key="hide_original_checkbox")
-#         hide_original = st.checkbox("Hide raw data", value=True, key="hide_original_checkbox")
 
 
 
+        
 st.markdown("---")
 st.markdown("#")
+
+
 
 
 mobile = st.checkbox("Mobile", value=False)
@@ -116,47 +120,42 @@ st.markdown("##")
 submit = st.button("Submit")
 
 
-# st.markdown("# ")
-# st.markdown("# ")
-
-# chart = st.empty()
-
-
 
 
 if submit:
-    if mobile:
-        hide_element("button", "title", "View fullscreen")
+#     if mobile:
+#         hide_element("button", "title", "View fullscreen")
     
 #     if saronite_value:
 #         chart = st.altair_chart(plot_saronite_value_history(server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
-    else:
+#     else:
         
-        st.markdown("# ")
-        st.markdown("# ")
-        
-        a, title, c = st.columns(3)
-        with title:
-            st.markdown(f"### {item.title()}")
-        
-        st.markdown("# ")
-        chart = st.empty()
-        
-        if chart_type == "Price":
-            if server_compare is not None and faction_compare is not None:
-                chart = st.altair_chart(plot_price_history_comparison(item, server, faction, server_compare, faction_compare, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
-            else:
-                chart = st.altair_chart(plot_price_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
+    st.markdown("# ")
+    st.markdown("# ")
+
+#     a, title, c = st.columns(3)
+#     with title:
+    st.markdown(f"### {item.title()}")
+
+    st.markdown("# ")
+    chart = st.empty()
+
+    if chart_type == "Price":
+        if server_compare is not None and faction_compare is not None:
+            chart = st.altair_chart(plot_price_history_comparison(item, server, faction, server_compare, faction_compare, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
+        else:
+            chart = st.altair_chart(plot_price_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
 
 
-        elif chart_type == "Price & Quantity":
-            chart = st.altair_chart(plot_price_and_quantity_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
+    elif chart_type == "Price & Quantity":
+        chart = st.altair_chart(plot_price_and_quantity_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
 
 
-        elif chart_type == "Price & Region Price":
-            chart = st.altair_chart(plot_price_and_region_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
+    elif chart_type == "Price & Region Price":
+        chart = st.altair_chart(plot_price_and_region_history(item, server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
 
-
+    if mobile:
+        hide_element("button", "title", "View fullscreen")
 
         
         
@@ -165,8 +164,7 @@ if submit:
 # Hides the Submit button if on mobile
 # run_custom_css(""" @media (pointer:none),(pointer:coarse) { button[kind="secondary"] {display: none;} } """)        
         
-# Hides the "Made with Streamlit" footer element
-hide_element("footer", "class", "css-1lsmgbg egzxvld0")
+
         
 # run_custom_javascript(
 # """
