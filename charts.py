d56711bf3c8dbc576e0f31f1e2d0c79cb3bf08c0
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-02-19 21:05:51 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -673,7 +673,7 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
         )
         st.markdown(f"**Error:** {e}")
     
-    
+    st.write(chart_ylims)
     
     if not hide_original:
         range_quantity = [data["Quantity"].min(), data["Quantity"].max()]
