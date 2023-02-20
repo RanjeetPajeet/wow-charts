3730dc3c55e7187a84b1972515582e87846bd95c
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-02-19 20:57:00 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -764,8 +764,8 @@ def plot_price_and_quantity_history(item: str, server: str, faction: str, num_da
         # make a second price line but with zero opacity (invisible)
         price_line_ma12_2 = alt.Chart(data).mark_line(
             color = "#6029c1",
-            strokeWidth = 5.1,
-            opacity = 0,
+            strokeWidth = 20.1,
+            opacity = 0.5,
         ).encode(
             x=alt.X("Time", axis=alt.Axis(title="Date", format=XAXIS_DATETIME_FORMAT)),
             y=alt.Y("12-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims)),
