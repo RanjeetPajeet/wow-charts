ec9b8c785fdfae3e083bf727c706cfc252392d8c
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-02-19 21:38:21 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -441,7 +441,7 @@ def plot_price_history(item: str, server: str, faction: str, num_days: int, ma4:
         if hide_original:
             # make a second price line but with zero opacity
             # to assist in tooltip visibility when mousing over
-            price_line_ma12_mouseover = alt.Chart(data).mark_line(
+            price_line_mouseover = alt.Chart(data).mark_line(
                 color = "#7defa1",
                 strokeWidth = MOUSEOVER_LINE_THICKNESS,
                 opacity = 0.3,
@@ -453,7 +453,7 @@ def plot_price_history(item: str, server: str, faction: str, num_days: int, ma4:
             chart = alt.Chart(data).mark_area(fill='red', opacity=0, strokeWidth=2, clip=True, line=True).encode(
                         color=alt.value("#7defa1"),
                         x=alt.X("Time", axis=alt.Axis(title="Date", format=XAXIS_DATETIME_FORMAT)),
-                        y=alt.Y("4-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims)))
+                        y=alt.Y("4-hour moving average", axis=alt.Axis(title=ylabel), scale=alt.Scale(domain=chart_ylims))) + price_line_mouseover
         else:
             chart = chart + alt.Chart(data).mark_line(color="#7defa1").encode(x=alt.X("Time"), y=alt.Y("4-hour moving average"))
     if ma12:
