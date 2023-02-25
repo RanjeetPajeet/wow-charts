e00077587a8c208917f3d88cacd00677d4593b68
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-02-25 11:43:14 -0600

    Update charts.py

--- charts.py
+++ charts.py
@@ -124,7 +124,8 @@ def create_OHLC_chart(OHLC_data: dict, minimum: float, maximum: float) -> alt.Ch
     range_quantity = [OHLC_df["mean_quantity"].min(), OHLC_df["mean_quantity"].max()]
     quantities = [ map_value(x, range_quantity, [chart_ylims[0],minimum]) for x in OHLC_df["mean_quantity"] ]
     quantities = [ ((x+minimum)/SCALE) for x in quantities ]
-    
+    st.write(minimum)
+    st.write(chart_ylims[0])
     
     OHLC_df.insert(2, "quantities", quantities, True)
     #st.write(quantities)
