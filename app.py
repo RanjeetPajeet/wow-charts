aed3fd182ec1d85c206847680d45ffeddcd2cf0c
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-03-01 07:09:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
-from api import api_offline
+from api import api_online, api_offline
+from misc import hide_element, titleize
 from data import get_server_history_OHLC
-from misc import run_custom_css, hide_element, run_custom_javascript, titleize
-from charts import create_OHLC_chart
-from charts import plot_price_history, plot_price_and_quantity_history, plot_price_and_region_history, plot_price_history_comparison, plot_saronite_value_history
-
-from streamlit.components.v1 import html
+from charts import plot_price_history, plot_price_and_quantity_history, plot_price_and_region_history, plot_price_history_comparison, create_OHLC_chart
 
 st.set_page_config(
     layout     = "centered",
@@ -13,177 +10,108 @@ st.set_page_config(
     page_title = "AH Prices",
 )
 
-# Hides the "Made with Streamlit" footer element
-hide_element("footer", "class", "css-1lsmgbg egzxvld0")
-
 
 st.title("Auction House Data")
 st.markdown("---")
 
-st.error("API is offline", icon="🚨")
 
 
 
 with st.container():
     st.markdown("### Parameters")
-    st.markdown("###")
-    
-    
-    
+    st.markdown("### ")
     
     item_col, days_col = st.columns(2)
-    
-    with item_col:
-        item = st.text_input("Item name", "Saronite Ore")
-    with days_col:
-        num_days = st.number_input("Number of days", 1, 730, 120)
-
+    with item_col:  item = st.text_input("Item name", "Saronite Ore")
+    with days_col:  num_days = st.number_input("Number of days", 1, 730, 120)
         
     server_col, faction_col = st.columns(2)
     with server_col:  server = st.selectbox("Server", ["Skyfury","Pagle","Atiesh","Faerlina","Whitemane"])
     with faction_col: faction = st.selectbox("Faction", ["Alliance","Horde"]) if server in ["Skyfury","Pagle","Atiesh"] else st.selectbox("Faction", ["Horde","Alliance"])
-        
-#     server_col, faction_col = st.columns(2)
-    
-#     with server_col:
-#         server = st.selectbox("Server", ["Skyfury","Pagle","Atiesh","Faerlina","Whitemane"])
-#     with faction_col:
-#         if server == "Skyfury" or server == "Atiesh" or server == "Pagle":
-#             faction = st.selectbox("Faction", ["Alliance","Horde"])
-#         else:
-#             faction = st.selectbox("Faction", ["Horde","Alliance"])
-    
-    
-    
-    
-    st.markdown("##")
 
-    
-    
-    
+    st.markdown("## ")
+
     chart_type = st.selectbox("Chart type", ["Price & Quantity","Price","Price & Region Price"])
+    server_compare = None   # Need to initialize as `None` to avoid exception when determining if the `candlestick` checkbox should be drawn
 
 
-    server_compare = None
-    
     if chart_type == "Price":
         server_col_compare, faction_col_compare = st.columns(2)
-        
         with server_col_compare:
             server_compare = st.selectbox("Compare with", [None,"Pagle","Atiesh","Skyfury","Faerlina","Whitemane"], key="server_compare")
-        
         with faction_col_compare:
             if server_compare is not None:
                 if server_compare == server:
                     faction_compare = st.selectbox(" ", [f for f in ["Alliance","Horde"] if f != faction], key="faction_compare", disabled=True)
-                elif server_compare == "Skyfury" or server_compare == "Atiesh" or server_compare == "Pagle":
+                elif server_compare == "Skyfury" or server_compare == "Atiesh":
                     faction_compare = st.selectbox(" ", ["Alliance","Horde"], key="faction_compare")
                 else:
                     faction_compare = st.selectbox(" ", ["Horde","Alliance"], key="faction_compare")
             else:
                 faction_compare = st.selectbox(" ", [None,"Alliance","Horde"], key="faction_compare", disabled=True)
+                
 
-                
-                
-                
-    st.markdown("#")
+    st.markdown("# ")
     st.markdown("### Moving averages")
-    st.markdown("###")
+    st.markdown("### ")
     
 
-    
-    
-    ma_col4, ma_col12, ma_col24, ma_col48, hide_og_col = st.columns(5)
-    with ma_col4:
-        ma4 = st.checkbox("4 hour", value=False, key="ma4_checkbox")
-    with ma_col12:
-        ma12 = st.checkbox("12 hour", value=True, key="ma12_checkbox")
-    with ma_col24:
-        ma24 = st.checkbox("24 hour", value=False, key="ma24_checkbox")
-    with ma_col48:
-        ma48 = st.checkbox("48 hour", value=False, key="ma48_checkbox")
-    with hide_og_col:
-        hide_original = st.checkbox("Hide raw", value=True, key="hide_original_checkbox")
+    ma4_col, ma12_col, ma24_col, ma48_col, hideOG_col = st.columns(5)
+    with ma4_col:    ma4  = st.checkbox("4 hour",  value=False, key="ma4_checkbox")
+    with ma12_col:   ma12 = st.checkbox("12 hour", value=True,  key="ma12_checkbox")
+    with ma24_col:   ma24 = st.checkbox("24 hour", value=False, key="ma24_checkbox")
+    with ma48_col:   ma48 = st.checkbox("48 hour", value=False, key="ma48_checkbox")
+    with hideOG_col: hide_original = st.checkbox("Hide raw", value=True, key="hide_original_checkbox")
 
 
 
-        
 st.markdown("---")
-st.markdown("#")
 
 
 
-
+st.markdown("# ")
 mobile = st.checkbox("Mobile", value=False)
-st.markdown("##")
 
-if chart_type in ["Price","Price & Quantity"] and server_compare is None:
-    candlestick = st.checkbox("Candlestick", value=False)
-    st.markdown("##")
-else: candlestick = False
+st.markdown("## ")
+candlestick = st.checkbox("Candlestick", value=False)
 
-# saronite_value = st.checkbox("Saronite Value", value=False)
-# st.markdown("##")
-
-
+st.markdown("## ")
 submit = st.button("Submit")
 
+st.markdown("## ")
 chart = st.empty()
 
 
 
 if submit:
-#     if mobile:
-#         hide_element("button", "title", "View fullscreen")
-    
-#     if saronite_value:
-#         chart = st.altair_chart(plot_saronite_value_history(server, faction, num_days, ma4, ma12, ma24, hide_original, mobile), use_container_width=True)
-    
-    
-    if candlestick:
-        st.markdown("## ")
-        with st.spinner("Loading..."):
-            ohlc_data, data_min, data_max = get_server_history_OHLC(item, server, faction, num_days)
-            #st.markdown("# ")
+    try:
+        if candlestick:
+            with st.spinner("Loading..."):
+                ohlc_data, data_min, data_max = get_server_history_OHLC(item, server, faction, num_days)
+                st.markdown("# ")
+                st.markdown("# ")
+                st.markdown(f"### [{titleize(item)}] {chart_type} -- Last {num_days} Days")
+                st.markdown("## ")
+                chart = st.altair_chart(create_OHLC_chart(ohlc_data, data_min, data_max, mobile=mobile))
+        else:
+            st.markdown("# ")
+            st.markdown("# ")
+            st.markdown(f"### [{titleize(item)}] {chart_type} -- Last {num_days} Days")
             st.markdown("## ")
-            st.markdown(f"### [{titleize(item)}] {chart_type} History -- Last {num_days} Days")
-            st.markdown("## ")
-            chart = st.altair_chart(create_OHLC_chart(ohlc_data, data_min, data_max, mobile=mobile))
-    else:
-        st.markdown("# ")
-        st.markdown("# ")
-        st.markdown(f"### [{titleize(item)}] {chart_type} History -- Last {num_days} Days")
-        st.markdown("## ")
-
-        if chart_type == "Price":
-            if server_compare is not None and faction_compare is not None:
-                chart = st.altair_chart(plot_price_history_comparison(item, server, faction, server_compare, faction_compare, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
-            else:
-                chart = st.altair_chart(plot_price_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
-
-        elif chart_type == "Price & Quantity":
-            chart = st.altair_chart(plot_price_and_quantity_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
-
-        elif chart_type == "Price & Region Price":
-            chart = st.altair_chart(plot_price_and_region_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
-
+            
+            chart = st.empty()
+            if chart_type == "Price":
+                if server_compare is not None and faction_compare is not None:
+                    chart = st.altair_chart(plot_price_history_comparison(item, server, faction, server_compare, faction_compare, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
+                else: chart = st.altair_chart(plot_price_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
+            elif chart_type == "Price & Quantity":
+                chart = st.altair_chart(plot_price_and_quantity_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
+            elif chart_type == "Price & Region Price":
+                chart = st.altair_chart(plot_price_and_region_history(item, server, faction, num_days, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
+        
         if mobile:
             hide_element("button", "title", "View fullscreen")
+    
+    except Exception as e:
+        st.error("NexusHub API is currently offline.", icon="🚨") if api_offline() else st.markdown(f"### Error: {e}")
 
-        
-        
-
-        
-# Hides the Submit button if on mobile
-# run_custom_css(""" @media (pointer:none),(pointer:coarse) { button[kind="secondary"] {display: none;} } """)        
-        
-
-        
-# run_custom_javascript(
-# """
-# var elements = document.querySelectorAll("a[href='https://streamlit.io/cloud']");
-# for (var i = 0, l = elements.length; i < l; i++) {
-#     var element = elements[i];
-#     element.style.visibility = "hidden";
-# }
-# """)
