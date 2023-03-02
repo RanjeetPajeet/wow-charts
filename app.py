7a18cd02fb7f386010100548521d3ef46d07e578
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-03-02 16:16:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -103,13 +103,12 @@ if submit:
                 ohlc_data, data_min, data_max = get_server_history_OHLC(item, server, faction, num_days)
                 title(item, chart_type, num_days)
                 chart = st.altair_chart(create_OHLC_chart(ohlc_data, data_min, data_max, mobile=mobile))
-#         elif chart_type == "Price":
-#             if server_compare is None and faction_compare is None:
-#                 st.write("Here")
-#                 with st.spinner("Loading..."):
-#                     price_data = get_server_history(item, server, faction, num_days)
-#                     title(item, chart_type, num_days)
-#                     chart = st.altair_chart(Plot.price_history(price_data, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
+        elif chart_type == "Price":
+            if server_compare is None and faction_compare is None:
+                with st.spinner("Loading..."):
+                    price_data = get_server_history(item, server, faction, num_days)
+                    title(item, chart_type, num_days)
+                    chart = st.altair_chart(Plot.price_history(price_data, ma4, ma12, ma24, ma48, hide_original, mobile), use_container_width=True)
 
         else:
             st.markdown("# ")
