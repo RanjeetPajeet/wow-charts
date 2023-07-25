88d715f60fd5757c455d362b62e5f1957deea435
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-07-25 15:28:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,9 @@ if submit:
         from wcltooltips import *
         from firebase_admin import credentials, db
         cred = credentials.Certificate("!FirebaseRealtimeDatabaseCredentials.json")
-        app = firebase_admin.initialize_app(cred, {"databaseURL": "https://wcltooltips-default-rtdb.firebaseio.com/"})
+        if "app" not in st.session_state:
+            st.session_state["app"] = firebase_admin.initialize_app(cred, {"databaseURL": "https://wcltooltips-default-rtdb.firebaseio.com/"})
+        app = st.session_state["app"]
         DB_REF = db.reference("/")
         
         pagle_dict = DB_REF.child("pagle_json").get()
